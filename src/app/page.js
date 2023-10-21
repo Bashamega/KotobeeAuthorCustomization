b20954cc008191b48c45cf7d916fb36a34b59a9f
Bashamega <adambashaahmednaji@gmail.com>
2023-10-21 08:50:50 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 "use client"
 import React from 'react';
-import { Nav } from '@/components/Nav';
-import Search from '@/components/Search';
-import { features } from '@/db/features';
-import { port } from '@/components/functions';
-import { Footer } from '@/components/footer';
+import { Nav } from '../components/Nav';
+import Search from '../components/search';
+import { features } from '../db/features';
+import { port } from '../components/functions';
+import { Footer } from '../components/footer';
 export default function Home() {
 
 
