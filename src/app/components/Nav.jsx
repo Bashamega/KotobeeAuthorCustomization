7b71211930cfe998ab3f59efa324eefceb0a2079
Bashamega <adambashaahmednaji@gmail.com>
2023-10-21 11:46:16 +0300

    Update Nav.jsx

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -4,7 +4,7 @@ import { port } from "./functions";
 export function Nav() {
   return (
     <nav className="bg-[#503e52] text-white flex items-center p-5">
-      <img src={port + "/favicon.ico"} width="50px" alt="Logo" />
+      <a href={port}><img src={port + "/favicon.ico"} width="50px" alt="Logo" /></a>
       <h1 className="text-xl mx-auto">Kotobee Author Customization</h1>
       <Search></Search>
     </nav>
